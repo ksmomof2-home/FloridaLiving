f9d611833d24d132515c6e7c1461b10598ae10b1
ksmomof2-home <kellyterri@verizon.net>
2025-11-28 20:35:26 -0500

    Create image-list.js Need to Update image names

---
+++ docs/js/image-list.js
@@ -0,0 +1,8 @@
+// js/image-list.js  – auto-generated, but you can also maintain it manually for now
+window.GARDEN_IMAGE_LIST = [
+  "pot01_20251126.jpg",
+  "pot01_20251001.jpg",
+  "pot07_20251115_flowers.jpg",
+  "pot20_20251120.jpg",
+  // … one line per photo
+];
